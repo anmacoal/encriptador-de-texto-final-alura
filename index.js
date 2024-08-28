1d694c3ff071e4b00e0d68a15ca2156736162dd1
anmacoal <anmacoal78@gmail.com>
2024-08-27 20:22:04 -0500

    version final encriptador de texto

---
+++ index.js
@@ -0,0 +1,56 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+       .replace(/e/gi, "enter")
+       .replace(/i/gi, "imes")
+       .replace(/a/gi, "ai")
+       .replace(/o/gi, "ober")
+       .replace(/u/gi, "ufat");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto encriptado con exito";
+        parrafo.textContent = "";
+        muñeco.src ="./img/imagendos.png";        
+    }   else  {
+        muñeco.src = "./img/jshtmlcss.jpg";
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algun texto");
+    }
+}
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "ae")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+
+      if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con exito";
+        parrafo.textContent = "";
+        muñeco.src ="./img/desencriptado.png";
+    }   else  {
+        muñeco.src = "./img/jshtmlcss.jpg";
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algun texto");
+
+        
+      }
+      
+    
+}
+
